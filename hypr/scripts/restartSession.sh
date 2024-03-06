87c73e610674095a72f03332ac8d9775615b0424
Jayden Lee Murray <jayden.murray@arrowhead.systems>
2024-03-06 11:26:51 -0500

    init

---
+++ hypr/scripts/restartSession.sh
@@ -0,0 +1,4 @@
+#!
+
+pkill waybar & pkill wpaperd
+waybar --config /home/$USER/.config/hypr/waybar/waybar.conf --style /home/$USER/.config/hypr/waybar/waybar_styles.css & wpaperd
---
+++ hypr/scripts/test.sh
@@ -0,0 +1,5 @@
+[default]
+path = /home/jaydenlm/.themes/Nord
+apply-shadow = false
+duration = 30m
+sorting = random
