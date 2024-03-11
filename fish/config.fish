b1be742c13f083b2b40878aeb76e8c798df4d996
Jayden Lee Murray <jayden.murray@arrowhead.systems>
2024-03-11 10:26:57 -0400

    joe

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+if status is-login
+    exec Hyprland
+end
+
 set fish_greeting
 
 cbonsai -l -t0.00001 -M5 -p
